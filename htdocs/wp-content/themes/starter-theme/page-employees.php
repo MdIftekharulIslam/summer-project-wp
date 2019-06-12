fd06986a2b3a74756f310b9dd964cf5ee878c7da
Aisha <aishahhassan21@gmail.com>
2019-06-12 15:59:46 +0300

    author and page employee updated

--- htdocs/wp-content/themes/starter-theme/page-employees.php
+++ htdocs/wp-content/themes/starter-theme/page-employees.php
@@ -3,6 +3,16 @@
 Template Name: Employees
 */
 
+$args = [
+    'fields' => [
+        'ID',
+        'display_name',
+        'user_email',
+    ]
+];
+	
+$users = get_users($args);
+
 the_post();
 
 $staff = get_field( 'staff_list' ); 
@@ -11,4 +21,42 @@ get_header();
 
 ?>
 
-This will be the employees page.
+<p>View our amazing team member from all departments in the organisation. </p>
+
+<?php if (! empty ($users)): ?>
+
+<ul>
+    <?php foreach( $users as $users_obj ) :
+        $user_key = 'user_' . $users_obj->ID;
+        $job_title = get_field( 'job_title', $user_key );
+        $phone = get_field( 'phone', $user_key );
+    ?>
+        <li>
+            <div class="thumb"><img src="<?php the_field( 'profile_picture', $user_key ); ?>"></div>
+			 <p class="info vcard">
+			    <span class="name">
+                    <a href="<?php echo esc_url( get_author_posts_url( $users_obj->ID ) ); ?>">
+                        <?php esc_html_e( $users_obj->display_name ) ; ?>
+                    </a>
+                </span>
+                
+                <?php if ( $job_title ) : ?>
+                    <span class="title"><?php esc_html_e( $job_title ); ?></span>
+                <?php endif; ?>
+                
+                <span class="email"><?php esc_html_e( $users_obj->user_email ); ?></span>
+                
+                <?php if ( $job_title ) : ?>
+                    <span class="phone"><?php esc_html_e( $phone ); ?></span>
+                <?php endif; ?>
+			</p> 
+        </li>
+    
+<?php endforeach; ?>    
+</ul>
+
+<?php endif; ?> 
+
+
+
+
