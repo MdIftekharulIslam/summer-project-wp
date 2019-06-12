b82533fa9dabfcf687476047325e95bf7309986c
dipendra <dipendra.dangol11@gmail.com>
2019-06-12 11:13:06 +0300

    Add ACF, add local ACF json, add function files, add header, add footer.

---
+++ htdocs/wp-content/themes/starter-theme/src/department.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * All code related to departments.
+ */
+
+//Custom post
+add_action('init', 'st_custom_post_type');
+function st_custom_post_type(){
+    register_post_type( 'department', 
+        array(
+            'labels'      => array(
+                'name'          => __('Departments'),
+                'singular_name' => __('Department'),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+        )
+    );
+}
---
+++ htdocs/wp-content/themes/starter-theme/src/setup.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Setup the theme.
+ */
+
+//Add support for featured images
+add_action( 'after_setup_theme', 'startertheme_setup_theme' );
+function startertheme_setup_theme(){
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'title-tag' );
+}
