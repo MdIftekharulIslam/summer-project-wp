259fd4819a25283078db1ae7c980759bbcebf2e2
dipendra <dipendra.dangol11@gmail.com>
2019-06-13 12:50:06 +0300

    added walker function for top menu in menus file, modified header, footer, index file

--- htdocs/wp-content/themes/starter-theme/footer.php
+++ htdocs/wp-content/themes/starter-theme/footer.php
@@ -1,3 +1,8 @@
 <?php wp_footer(); ?>
+    <div class="top-bar">
+        <div class="top-bar-left">
+       
+        <div class="top-bar-right"></div>
+    </div>
 </body>
 </html>
--- htdocs/wp-content/themes/starter-theme/src/menus.php
+++ htdocs/wp-content/themes/starter-theme/src/menus.php
@@ -5,3 +5,19 @@ function register_my_menu() {
 }
 
 add_action( 'init', 'register_my_menu' );
+
+/**
+ * Foundation 6 TopBar menu walker class.
+ * 
+ * @see https://github.com/wlcdesigns/foundation-6-wordpress-menu-walkers/blob/master/top-bar.php
+ */
+class F6TopBarMenuWalker extends Walker_Nav_Menu {   
+	/*
+	 * Add vertical menu class and submenu data attribute to sub menus
+	 */
+	 
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\" data-submenu>\n";
+	}
+}
